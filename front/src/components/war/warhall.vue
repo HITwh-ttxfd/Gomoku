6f7961581af8eefa64602222a775a03edaceef1d
王琪淞 <44025398+thomeal@users.noreply.github.com>
2020-07-04 23:12:34 +0800

    board

--- front/src/components/war/warhall.vue
+++ front/src/components/war/warhall.vue
@@ -6,11 +6,17 @@
         <div class="title">Gomoku</div>
       </div>
       <div class="right">
-        <el-button @click="dialogVisible = true" plain>返回大厅</el-button>
+        <el-button @click="$router.push({path: '/waithall'})" plain>返回大厅</el-button>
       </div>
     </div>
     <div class="body">
-
+      <div class="game">
+        <canvas id="canvas" height="600" width="600"></canvas>
+      </div>
+      <div class="chatting">
+        <div class="receive"></div>
+        <div class="send"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -20,13 +26,53 @@
   export default {
     name: "warhall",
     data() {
-      return {}
+      return {
+        square: [],
+        board: {}
+      }
     },
-    methods: {}
+    methods: {
+      loadCanvas() {
+        let canvas = document.getElementById('canvas');
+        this.board = canvas.getContext('2d');
+        let board = this.board;
+        board.strokeRect(20, 20, 560, 560);
+        for (let i = 0; i < 15; ++i) {
+          board.moveTo(20 + i * 40, 20);
+          board.lineTo(20 + i * 40, 580);
+          board.stroke();
+        }
+        for (let j = 0; j < 15; ++j) {
+          board.moveTo(20, 20 + j * 40);
+          board.lineTo(580, 20 + j * 40);
+          board.stroke();
+        }
+        board.beginPath();
+        board.arc(300, 300, 5, 0, 2*Math.PI);
+        board.fill();
+        board.beginPath();
+        board.arc(460, 460, 5, 0, 2*Math.PI);
+        board.fill();
+        board.beginPath();
+        board.arc(460, 140, 5, 0, 2*Math.PI);
+        board.fill();
+        board.beginPath();
+        board.arc(140, 140, 5, 0, 2*Math.PI);
+        board.fill();
+        board.beginPath();
+        board.arc(140, 460, 5, 0, 2*Math.PI);
+        board.fill();
+      }
+    },
+    mounted() {
+      this.loadCanvas();
+    }
   }
 </script>
 
 <style lang="scss" scoped>
+  $border: rgba(213, 213, 213, 1.000) 1px solid;
+  $boardLine: rgb(50, 49, 49) 2px solid;
   .wrapper {
     height: 100%;
 
@@ -34,7 +80,7 @@
       position: relative;
       background: rgba(247, 247, 247, 1.000);
       height: 70px;
-      border-bottom: rgba(213, 213, 213, 1.000) 1px solid;
+      border-bottom: $border;
       box-shadow: 0 3px 3px -3px #5E5E5E;
 
       .left {
@@ -62,6 +108,48 @@
         position: absolute;
         right: 10px;
         top: 15px;
+      }
+    }
+
+    .body {
+      position: relative;
+      display: flex;
+      height: 91.4%;
+
+      .game {
+        flex: 4;
+        height: 100%;
+        background: {
+          /*image: url("../../assets/img/1.jpg");*/
+          size: cover;
+        };
+        /*display: flex;*/
+
+        #canvas {
+          margin: 75px 200px;
+          background: url("../../assets/img/timg.jpg") no-repeat center;
+          background-size: cover;
+          border-radius: 5px;
+          box-shadow: 2px 2px 3px 5px rgba(227, 173, 120, 1.000);
+        }
+      }
+
+      .chatting {
+        flex: 1.5;
+        flex-direction: column;
+        display: flex;
+        border: {
+          left: $border;
+        };
+
+        .receive {
+          flex: 5;
+        }
+
+        .send {
+          flex: 1;
+          border-top: $border;
+        }
       }
     }
   }
